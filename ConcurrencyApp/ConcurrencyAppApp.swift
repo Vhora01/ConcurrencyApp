ceeffec234e880bddc2dd00246be88cf932fafbc
Prakash <vhoraprakash007@gmail.com>
2025-08-14 14:55:46 +0530

    Initial Commit

---
+++ ConcurrencyApp/ConcurrencyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConcurrencyAppApp.swift
+//  ConcurrencyApp
+//
+//  Created by Prakash on 14/08/25.
+//
+
+import SwiftUI
+
+@main
+struct ConcurrencyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
